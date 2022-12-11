0c47a75f0de48e1d7dd7aafb274e8e5dd782aa28
Oksana <ksenia79460@gmail.com>
2022-12-11 18:32:31 +0000

    styled app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleChangeTask = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: list === 0 ? 1 : [list.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setList(task.taskName !== "" ? [...list, task] : list);
+  };
+
   return (
     <div className="App">
-      
+      <div className="addTaskDiv">
+        <input
+          type="text"
+          placeholder="any task"
+          className="typeTask"
+          onChange={handleChangeTask}
+          value={newTask}
+        />
+        <button className="addTaskBtn" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <div className="listDiv">
+        {list.map((task) => {
+          return (
+            <div className="task">
+              <h2>{task.taskName}</h2>
+              <button>Complete</button>
+              <button>X</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
